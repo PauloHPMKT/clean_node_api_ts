2236fd926ed7bfaf1c64c39ff191d318cb1ad85d
Paulo Sergio <pauloserg861@gmail.com>
2023-12-27 19:29:34 -0300

    chore: add jest to lintstaged

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,12 @@ const config: Config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  clearMocks: true,
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'node',
   transform: {
     '^.+\\.(t|s)sx?$': '@swc/jest',
   },
   maxWorkers: '100%',
-  testMatch: ['**/test/**/*.test.ts'],
 };
 
 export default config;
